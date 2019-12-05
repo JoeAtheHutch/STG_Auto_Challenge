44f241ef6f79fb71e59a3079787ccc3ea8173b7b
JoeAtheHutch <joseph.hutch@dish.com>
2019-12-05 10:48:23 -0700

    Challenge2 v1.0

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -1,5 +1,8 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 class challenge2(unittest.TestCase):
 
@@ -10,13 +13,19 @@ class challenge2(unittest.TestCase):
         self.driver.close()
 
     def test_challenge2(self):
+        self.driver.get("https://www.copart.com")
         searchButton = self.driver.find_element_by_xpath("//*[@id='search-form']/div/div[2]/button")
         searchField = self.driver.find_element_by_id("input-search")
 
-        self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
-        self.search.send_keys("exotic")
-        self.searchButton.click()
+        searchField.send_keys("exotic")
+        searchButton.click()
+
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.visibility_of_element_located((
+                By.XPATH, '//*[@id="collapseinside3"]')))
+        makeField = self.driver.find_element_by_xpath('//*[@id="collapseinside3"]').text
+        self.assertIn("Porsche", makeField)
 
 
 if __name__ == '__main__':
