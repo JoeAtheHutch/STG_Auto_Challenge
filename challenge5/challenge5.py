59f3ba153a114224e4003571cf64a386ae4e5497
JoeAtheHutch <joseph.hutch@dish.com>
2020-01-13 15:08:43 -0700

    adding challenge 5

--- challenge5/challenge5.py
+++ challenge5/challenge5.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import Select
+from common.TopNavSearch import topNavSearch
 
 class challenge2(unittest.TestCase):
 
@@ -15,15 +17,19 @@ class challenge2(unittest.TestCase):
         self.driver.close()
 
     def test_challenge5(self):
-        searchField = self.driver.find_element(By.ID, "input-search")
-        searchField.send_keys("porsche")
-        searchButton = self.driver.find_element(By.XPATH, '//*[@id="search-form"]/div/div[2]/button')
-        searchButton.click()
+        searchBar = topNavSearch(self.driver)
+        searchBar.runSearch("ford")
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.visibility_of_element_located((
+                By.XPATH, '//*[@id="serverSideDataTable_length"]/label/select')))
+        resultNumber = self.driver.find_element(By.XPATH, '//*[@id="serverSideDataTable_length"]/label/select')
+        Select(resultNumber).select_by_visible_text('100')
+        time.sleep(5)
+
+        
         
         
         
 
 if __name__ == '__main__':
     unittest.main()
-
-
---
+++ challenge5/common/TopNavSearch.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+class topNavSearch:
+    def __init__(self, driver):
+        self.driver = driver
+
+
+    def runSearch(self, searchString):
+        searchField = self.driver.find_element(By.ID, "input-search")
+        searchField.send_keys(searchString)
+        searchButton = self.driver.find_element(By.XPATH, '//*[@id="search-form"]/div/div[2]/button')
+        searchButton.click()
