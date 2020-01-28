b8b785b8bbf63798c9ec90a6091c29588ac235ea
JoeAtheHutch <joseph.hutch@dish.com>
2020-01-28 15:29:58 -0700

    Updates for Challenge3 v1.1

--- challenge3/challenge3.py
+++ challenge3/challenge3.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-class challenge2(unittest.TestCase):
+class challenge3(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("../chromedriver")
@@ -12,8 +12,7 @@ class challenge2(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-    def test_challenge3forLoop(self):
-        self.driver.get("https://www.copart.com")
+    def test_challenge7(self):
         popularList = self.driver.find_elements(By.XPATH, '//*[@ng-if="popularSearches"]//a')
         for make in popularList:
             print(make.text + " - " + make.get_attribute("href"))
