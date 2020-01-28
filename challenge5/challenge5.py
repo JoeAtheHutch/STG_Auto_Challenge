140a910b4ab7cd8b6a71945d55533a42bd47a770
JoeAtheHutch <joseph.hutch@dish.com>
2020-01-28 15:30:41 -0700

    updates for challenge 5 v1.2

--- challenge5/challenge5.py
+++ challenge5/challenge5.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from common.TopNavSearch import topNavSearch
 from collections import Counter
@@ -23,31 +23,31 @@ class challenge2(unittest.TestCase):
         searchBar = topNavSearch(self.driver)
         searchBar.runSearch("porsche")
         WebDriverWait(self.driver, 5).until(
-            expected_conditions.visibility_of_element_located((
+            EC.visibility_of_element_located((
                 By.XPATH, '//*[@id="serverSideDataTable_length"]/label/select')))
         resultNumber = self.driver.find_element(By.XPATH, '//*[@id="serverSideDataTable_length"]/label/select')
         Select(resultNumber).select_by_visible_text('100')
         WebDriverWait(self.driver, 5).until(
-            expected_conditions.visibility_of_element_located((
+            EC.visibility_of_element_located((
                 By.XPATH, '//*[@id="serverSideDataTable"]/tbody/tr[50]//span')))
         models = self.driver.find_elements(By.XPATH, '//*[@id="serverSideDataTable"]/tbody/tr/td[6]/span')
         modelCount = countList(models)
         countedList = modelCount.getCounts()
         for model in countedList:
             print(model + " - " + str(countedList[model]))
-        print("****end model counts****")
+        print("****end model counts****\n")
         
     def test_damages(self):
         damageTypes = {"REAR END":0, "FRONT END":0, "MINOR DENT/SCRATCHES":0, "UNDERCARRIAGE":0, "MISC.":0}
         searchBar = topNavSearch(self.driver)
         searchBar.runSearch("porsche")
         WebDriverWait(self.driver, 5).until(
-            expected_conditions.visibility_of_element_located((
+            EC.visibility_of_element_located((
                 By.XPATH, '//*[@id="serverSideDataTable_length"]/label/select')))
         resultNumber = self.driver.find_element(By.XPATH, '//*[@id="serverSideDataTable_length"]/label/select')
         Select(resultNumber).select_by_visible_text('100')
         WebDriverWait(self.driver, 5).until(
-            expected_conditions.visibility_of_element_located((
+            EC.visibility_of_element_located((
                 By.XPATH, '//*[@id="serverSideDataTable"]/tbody/tr[50]//span')))
         damages = self.driver.find_elements(By.XPATH, '//*[@id="serverSideDataTable"]/tbody/tr/td[12]/span')
         whichType = damageSwitch()
