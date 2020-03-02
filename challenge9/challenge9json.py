ba9a1a74c6abee391124223e0ce059e795237f60
JoeAtheHutch <joseph.hutch@dish.com>
2020-03-02 09:43:07 -0700

    Adding structure for more challenges

---
+++ challenge9/challenge9json.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import unittest
+class challenge9json(unittest.TestCase):
+    def test_json(self):
+        url = "https://deckofcardsapi.com/deck/new/shuffle/?deck_count=1"
+
+        print(jo["remaining"])
+        # {
+            # "success": true,
+            # "deck_id": 
+        # }
